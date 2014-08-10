e3641ff52e6796c67c0034c5d346abe4114869a8
Rinfon <576983051@qq.com>
2014-08-10 22:57:22 +0800

    游戏动画设置

--- src/com/poker/common/activity/GameActivity.java
+++ src/com/poker/common/activity/GameActivity.java
@@ -1,24 +1,33 @@
 package com.poker.common.activity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.poker.common.R;
+import com.poker.common.entity.AbsPlayer;
+import com.poker.common.entity.Poker;
 import com.poker.common.entity.Room;
 
 @SuppressLint("NewApi")
 public class GameActivity extends Activity implements OnClickListener{
-	private ImageButton reback,follow,add,quit,tips;
+	private ImageButton reback;
+	private Button follow,add,quit,tips;
 	private TextView current_rank;
+	private ImageView img_card_tip;
 //	座位一永远都是自己
 	private LeftSeatView seat_two,seat_four,seat_six;
 	private RightSeatView seat_one,seat_three,seat_five;
@@ -26,6 +35,10 @@ public class GameActivity extends Activity implements OnClickListener{
 	private Animation public_poker_anim;
 //房间信息
 	private Room room;
+//	公共牌
+	private ArrayList<Poker> public_poker;
+//	玩家列表抽象，因为存在clientplayer和serverplayer，到时再继续详细的实例
+	private HashMap<String, AbsPlayer> playList;
 	
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +52,10 @@ public class GameActivity extends Activity implements OnClickListener{
 	private void init() {
 		// TODO Auto-generated method stub
 		reback = (ImageButton)findViewById(R.id.reback);
-		follow = (ImageButton)findViewById(R.id.follow);
-		add = (ImageButton)findViewById(R.id.add);
-		quit = (ImageButton)findViewById(R.id.quit);
-		tips = (ImageButton)findViewById(R.id.tips);
+		follow = (Button)findViewById(R.id.follow);
+		add = (Button)findViewById(R.id.add);
+		quit = (Button)findViewById(R.id.quit);
+		tips = (Button)findViewById(R.id.tips);
 		current_rank = (TextView)findViewById(R.id.player_current_rank);
 		seat_one = (RightSeatView)findViewById(R.id.seat_one);
 		seat_one.setPokerStyle(1);
@@ -58,11 +71,13 @@ public class GameActivity extends Activity implements OnClickListener{
 		public_poker3 = (ImageView)findViewById(R.id.poker3);
 		public_poker4 = (ImageView)findViewById(R.id.poker4);
 		public_poker5 = (ImageView)findViewById(R.id.poker5);
+		img_card_tip = (ImageView)findViewById(R.id.img_card_tip);
 		reback.setOnClickListener(this);
 		follow.setOnClickListener(this);
 		add.setOnClickListener(this);
 		quit.setOnClickListener(this);
 		tips.setOnClickListener(this);
+		img_card_tip.setVisibility(View.INVISIBLE);
 		setPublicPokerVisibility(View.INVISIBLE);
 	}	
 	
@@ -129,6 +144,7 @@ public class GameActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		switch(v.getId()){
 		case R.id.add:
+			closeCardTip();
 			bottomDeal();
 			break;
 		case R.id.reback:
@@ -137,15 +153,38 @@ public class GameActivity extends Activity implements OnClickListener{
 			finish();
 			break;
 		case R.id.follow:
+			closeCardTip();
 			resetAllPlayStatus();
 			break;
 		case R.id.quit:
+			closeCardTip();
 			showPublicPoker();
 			break;
 		case R.id.tips:
+			setCardTip(View.VISIBLE);
 			break;
 		default:
 			break;
 		}
 	}
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if(img_card_tip.getVisibility() == View.VISIBLE){
+			setCardTip(View.INVISIBLE);
+		}
+		return false;
+//		return super.onTouchEvent(event);
+	}
+	
+	public void setCardTip(int i){
+		img_card_tip.setVisibility(i);
+	}
+	
+	public void closeCardTip(){
+		if(img_card_tip.getVisibility() == View.VISIBLE){
+			img_card_tip.setVisibility(View.INVISIBLE);
+		}
+	}
+	
 }
