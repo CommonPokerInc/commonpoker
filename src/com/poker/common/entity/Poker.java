fcf58a4ff56511689630feb85746552db9dc9c4e
Rinfon <576983051@qq.com>
2014-08-11 23:41:10 +0800

    算法

--- src/com/poker/common/entity/Poker.java
+++ src/com/poker/common/entity/Poker.java
@@ -11,57 +11,84 @@ import android.R.bool;
 public class Poker {
 
 	//扑克牌花色
-	private String color;
+//	0：方块
+//	1：梅花
+//	2：红桃
+//	3：黑桃
+	private int color;
 	//大小
 	private int size;
 	//编号
 	private int number;
 	
-	public Poker(String color,int size) {
-		// TODO Auto-generated constructor stub
-		super();
-		initNum(color,size);
+//	public Poker(String color,int size) {
+//		// TODO Auto-generated constructor stub
+//		super();
+//		initNum(color,size);
+//	}
+	
+//	根据number来初始化扑克牌
+//	0-12方块
+//	13-25梅花 
+//	26-38红桃 
+//	39-21黑桃
+	public Poker(int number){
+		this.number = number;
+		this.color = number / 13;
+		this.size = number % 13;
+	}
+	
+	public Poker(Poker p){
+		this.number = p.getNumber();
+		this.color = p.getColor();
+		this.size = p.getSize();
+	}
+	
+	public void setPoker(Poker p){
+		this.number = p.getNumber();
+		this.color = p.getColor();
+		this.size = p.getSize();
 	}
 
-	private void initNum(String color, int size) {
+//	private void initNum(String color, int size) {
 		// TODO Auto-generated method stub
-		int c=0;
-		if("方块".equals(color)){
-			c=1;
-		}else if("梅花".equals(color)){
-			c=2;
-		}else if("红桃".equals(color)){
-			c=3;
-		}else{
-			c=4;
-		}
-		switch(Integer.valueOf(c)){
-		//方块
-		case 1:
-			number = (size%14)*4-3;
-			break;
-	    //梅花
-		case 2:
-			number = (size%14)*4-2;
-			break;
-		//红桃
-		case 3:
-			number = (size%14)*4-1;
-			break;
-		//黑桃
-		case 4:
-			number = (size%14)*4;
-			break;
-		default:
-			break;
-		}
-	}
+//		int c=0;
+//		if("方块".equals(color)){
+//			c=1;
+//		}else if("梅花".equals(color)){
+//			c=2;
+//		}else if("红桃".equals(color)){
+//			c=3;
+//		}else{
+//			c=4;
+//		}
+//		switch(Integer.valueOf(c)){
+//		//方块
+//		case 1:
+//			number = (size%14)*4-3;
+//			break;
+//	    //梅花
+//		case 2:
+//			number = (size%14)*4-2;
+//			break;
+//		//红桃
+//		case 3:
+//			number = (size%14)*4-1;
+//			break;
+//		//黑桃
+//		case 4:
+//			number = (size%14)*4;
+//			break;
+//		default:
+//			break;
+//		}
+//	}
 
-	public String getColor() {
+	public int getColor() {
 		return color;
 	}
 
-	public void setColor(String color) {
+	public void setColor(int color) {
 		this.color = color;
 	}
 
--- src/com/poker/common/util/Util.java
+++ src/com/poker/common/util/Util.java
@@ -1,5 +1,72 @@
 package com.poker.common.util;
 
-public class Util {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import com.poker.common.entity.Poker;
+
+public class Util {
+//	随机生成根据人数来定的扑克牌
+	public static List getPokers(int personsCount){
+		List pokers = new ArrayList(); 
+		int pokerLenght = personsCount*2+5;
+		Random rd = new Random();
+		int num ;
+		for(int i = pokerLenght - 1;i>=0;i--){
+			num = rd.nextInt(51);
+			if(!pokers.contains(num)){
+				pokers.add(num);
+			}else{
+				i++;
+			}
+		}
+		return pokers;
+	}
+//	根据玩家的7张牌得到最大的牌型，数字越大，牌型越大。
+	public static int getPokerType(ArrayList<Poker> pokers){
+		insertSort(pokers);
+		return 0;
+	}
+	
+//	判断是否存在5张花色相同
+	public static boolean checkoutFivePokerColorSame(ArrayList<Poker> pokers){
+		for(int i = pokers.size();i>=0;i--){
+			
+		}
+		return false;
+	}
+	
+//	判断是否存在5张牌数字连续
+	public static boolean chekcoutPokerSucceedingNumbers(ArrayList<Poker> pokers){
+		return false;
+	}
+	
+//	判断是否存在4张牌数字相同
+	public static boolean chekcoutFourPokerNumber(ArrayList<Poker> pokers){
+		return false;
+	}
+	
+//	判断是否存在3张牌数字相同
+	public static boolean checkoutThreePokerNumber(ArrayList<Poker> pokers){
+		return false;
+	}
+	
+//	判断是否存在2张牌数字相同
+	public static boolean checkoutTwoPokerNumber(ArrayList<Poker> pokers){
+		return false;
+	}
+	
+	public static ArrayList<Poker> insertSort(ArrayList<Poker> pokers){//插入排序算法
+        for(int i=0;i<pokers.size();i++){
+                for(int j=i;j>0;j--){
+                        if (pokers.get(j).getSize()<pokers.get(j-1).getSize()){
+                                Poker temp = new Poker(pokers.get(j-1));
+                                pokers.get(j-1).setPoker(pokers.get(j));
+                                pokers.get(j).setPoker(temp);
+                        }else break;
+                }
+        }
+        return pokers;
+}
 }
